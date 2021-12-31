e53380a8f6392d09dd0e440df46baf1c63421925
Mathieu Rodic <graphql@rodic.fr>
2021-12-31 07:58:20 +0100

    Refactored ORM handling, part II

--- src/easy_graphql/__init__.py
+++ src/easy_graphql/__init__.py
@@ -2,6 +2,13 @@
     Easy_GraphQL is an interface to easily expose a database in GraphQL via ORM models.
 """
 
+# backwards compatibility with older Python version
+if not hasattr(dict, '__ior__'):
+    def dict__ior__(self, other):
+        """ backwards compatibility with older Python version, dict |= other_dict """
+        for key, value in other.items():
+            self[key] = value
+    dict.__ior__ = dict__ior__
 
 from .types import *
 from .schema import Schema
