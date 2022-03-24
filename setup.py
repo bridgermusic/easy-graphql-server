78f3b873d11a8f40e49e514bc5b2efb52bb18cfb
Mathieu Rodic <graphql@rodic.fr>
2022-03-24 14:33:39 +0100

    Fixed static file

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@
 from re import search
 from setuptools import setup, find_packages
 
-version = '0.8.1a0',
+version = '0.9.2',
 
 with open('README.md') as readme_file:
     readme = readme_file.read()
 
 setup(
     name='easy_graphql_server',
-    version = '0.8.1a0',
+    version = '0.9.2',
     description='Easy to use abstraction layer for GraphQL, with support for Django ORM.',
     long_description=readme,
     long_description_content_type='text/markdown',
@@ -47,7 +47,10 @@ setup(
     packages=find_packages('src'),
     package_dir={'': 'src'},
     # PEP-561: https://www.python.org/dev/peps/pep-0561/
-    package_data={'easy_graphql_server': ['py.typed']},
+    package_data={
+        'easy_graphql_server': ['py.typed'],
+        'easy_graphql_server.webserver.static': ['*'],
+    },
     include_package_data=True,
     zip_safe=False,
 )
